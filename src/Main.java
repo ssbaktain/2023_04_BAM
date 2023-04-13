91f826e431b5caef2870a3541bea6694c77d00bb
ssbaktain <ssbaktain12@gmail.com>
2023-04-13 21:16:35 +0900

    게시물 작성 기능 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	public static int writingCount = 0;
+	
+	public static List<Article> articles;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		articles = new ArrayList<Article>();
 		String cmd = "";
 		
 		System.out.println("== 프로그램 시작 ==");
@@ -13,10 +20,32 @@ public class Main {
 			
 			if(cmd.equals("exit")) break;
 			
+			if(cmd.equals("article write")) {
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				articles.add(new Article(++writingCount, title, body));
+				System.out.println(articles.get(articles.size() - 1).id + "번 글이 생성되었습니다.");
+			}
+			
 			
 		}
 		sc.close();
 		
 		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
