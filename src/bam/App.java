dc31884886ebe9562eca7dbde0297437d42693ea
ssbaktain <ssbaktain12@gmail.com>
2023-04-17 21:42:08 +0900

    article list 기능 및 isNumber 기능 구현

--- src/bam/App.java
+++ src/bam/App.java
@@ -44,8 +44,11 @@ public class App {
 			articles.add(article);
 			System.out.println(article.id + "번 글이 생성되었습니다.");
 		} else if (cmd.startsWith("article modify ")) {
-			int target = Integer.parseInt(cmd.split(" ")[2]);
-
+			int target = getTarget(cmd);
+			if (target == -1) {
+				System.out.println("'article modify' 뒤에는 숫자만 올 수 있습니다.");
+				continue;
+			}
 			Article article = foundArticleById(target);
 
 			if (article == null) {
@@ -56,7 +59,6 @@ public class App {
 			while (true) {
 				System.out.print(">> ");
 				String modifyTarget = sc.nextLine();
-
 				if (modifyTarget.equals("제목")) {
 					System.out.print("수정할 제목 : ");
 					article.title = sc.nextLine();
@@ -76,33 +78,46 @@ public class App {
 			}
 			System.out.println(article.id + "번 게시물이 수정되었습니다.");
 		} else if (cmd.startsWith("article delete ")) {
-			int target = Integer.parseInt(cmd.split(" ")[2]);
-
+			int target = getTarget(cmd);
+			if (target == -1) {
+				System.out.println("'article delete' 뒤에는 숫자만 올 수 있습니다.");
+				continue;
+			}
 			int index = foundIndexById(target);
-
 			if (index == -1) {
 				System.out.println(target + "번 게시물은 존재하지 않습니다.");
 				continue;
 			}
 			articles.remove(index);
 			System.out.println(target + "번 게시물이 삭제되었습니다.");
-		} else if (cmd.equals("article list")) {
-			if (articles.size() == 0) {
+		} else if (cmd.startsWith("article list")) {
+			List<Article> foundList = new ArrayList<>();
+			if (cmd.startsWith("article list ")) {
+				String target = cmd.substring("article list".length()).trim();
+				for (Article article : articles) {
+					if (article.title.contains(target))
+						foundList.add(article);
+				}
+			} else foundList = articles;
+			
+			if (foundList.size() == 0) {
 				System.out.println("존재하는 게시물이 없습니다.");
 				continue;
 			}
 
 			System.out.println("== 게시물 목록 ==");
 			System.out.println("번호	|		작성일			|	제목");
-			for (int i = articles.size() - 1; i >= 0; i--) {
-				Article article = articles.get(i);
+			for (int i = foundList.size() - 1; i >= 0; i--) {
+				Article article = foundList.get(i);
 				System.out.printf("%d	|	%s	|	%s\n", article.id, article.regDate, article.title);
 			}
 		} else if (cmd.startsWith("article detail ")) {
-			int target = Integer.parseInt(cmd.split(" ")[2]);
-
+			int target = getTarget(cmd);
+			if (target == -1) {
+				System.out.println("'article detail' 뒤에는 숫자만 올 수 있습니다.");
+				continue;
+			}
 			Article article = foundArticleById(target);
-
 			if (article == null) {
 				System.out.println(target + "번 게시물은 존재하지 않습니다.");
 				continue;
@@ -141,5 +156,15 @@ public class App {
 		}
 		System.out.println("테스트용 게시물 데이터를 5개 생성하였습니다.");
 	}
+	
+	static int getTarget(String cmd) {
+		String targetStr = cmd.split(" ")[2];
+		if (isNumber(targetStr) == false)
+			return -1;
+		return Integer.parseInt(targetStr);
+	}
 
+	static boolean isNumber(String strValue) {
+		return strValue.matches("[-+]?\\d*\\.?\\d+");
+	}
 }
